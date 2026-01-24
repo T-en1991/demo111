7975b8a7f5a9f6bb90d11c32e517f0478d94edfe
Ten <940075665@qq.com>
2026-01-24 17:41:17 +0800

    feat: 1

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import TopBar from './components/TopBar.vue'
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import en from 'element-plus/es/locale/lang/en'
+import { useFishControlStore } from './store/fishControl'
 
 const { locale } = useI18n()
 const elementLocale = computed(() => {
   return locale.value === 'zh-CN' ? zhCn : en
+})
+
+const fishControlStore = useFishControlStore()
+
+onMounted(() => {
+  // 启动全局监听器
+  fishControlStore.initListeners()
 })
 </script>
 
